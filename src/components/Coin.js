17927121d943b0797c05a0708e9994cf3afd6947
cc <votaryofturms@icloud.com>
2021-12-09 02:03:37 -0500

    more data inputs into coin component

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { combineReducers } from "redux"
 export const Coin = (props) => {
     const { coin } = props
     return (
@@ -6,6 +7,14 @@ export const Coin = (props) => {
             <div>
                 <h3>{coin.name}</h3>
                 <img src={coin.image} />
+                <p>Abbreviation: {coin.symbol}</p>
+                <p>Market Cap: {coin.market_cap}</p>
+                <p>Current Price: {coin.current_price}</p>
+                <p>Circulating Supply: {coin.circulating_supply}</p>
+                <p>Total Supply: {coin.total_supply}</p>
+                <p>All Time High: {coin.ath}</p>
+                <p>24 Hour High: {coin.high_24h}</p>
+                <p>24 Hour Low: {coin.low_24h}</p>
             </div>
         </>
     )
