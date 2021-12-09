285bd7a7c4baf5a5efc5b2f6cdbd7de45567bb95
cc <votaryofturms@icloud.com>
2021-12-09 02:29:12 -0500

    applied styling

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -4,10 +4,11 @@ export const Coin = (props) => {
     const { coin } = props
     return (
         <>
-            <div>
+            <div className='coinContainer'>
                 <h3>{coin.name}</h3>
-                <img src={coin.image} />
+                <img className='coinLogos' src={coin.image} alt='coin symbol' />
                 <p>Abbreviation: {coin.symbol}</p>
+                <p>Market Cap Rank: {coin.market_cap_rank}</p>
                 <p>Market Cap: {coin.market_cap}</p>
                 <p>Current Price: {coin.current_price}</p>
                 <p>Circulating Supply: {coin.circulating_supply}</p>
